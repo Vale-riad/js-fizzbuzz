4474922e26867c8b33011af7b0a60408e2d79aab
Vale-riad <valentinariad91@gmail.com>
2022-10-24 19:12:19 +0200

    Aggiunge corr

--- js/main.js
+++ js/main.js
@@ -6,25 +6,26 @@ for (let i = 1; i <= 100; i++) {
     element.classList.add('board__number');
     element.innerHTML = i;
     div.append(element);
-    // 2.nei multipli di 3 viene stampato "Fizz"
-    if (i % 3 === 0) {
+    // 2.nei multipli di 15 viene stampato "FizzBuzz"
+     if (i % 15 === 0){
+        element.classList.add('fizzbuzz');
+        console.log(`FizzBuzz`)
+        element.innerHTML ="FizzBuzz"
+        // 4. nei multipli di 5 viene stampato "Buzz"
+    }else if (i % 5 === 0){
+        element.classList.add('buzz');
+        console.log(`Buzz`)
+        element.innerHTML ="Buzz"
+        // 3.nei multipli di 3 viene stampato "Buzz"
+    }if (i % 3 === 0) {
         element.classList.add('fizz');
    console.log(`Fizz`);
     element.innerHTML ="Fizz"
-    // 3.nei multipli di 5 viene stampato "Buzz"
-}else if (i % 5 === 0){
-    element.classList.add('buzz');
-    console.log(`Buzz`)
-    element.innerHTML ="Buzz"
-    // 4. nei multipli di 5 viene stampato "FizzBuzz"
-}else if (i % 15 === 0){
-    element.classList.add('fizzbuzz');
-    console.log(`FizzBuzz`)
-    element.innerHTML ="FizzBuzz"
-}else {
+    
     element.innerHTML = i;
     console.log(i);
 }
+ 
 }
 
 
