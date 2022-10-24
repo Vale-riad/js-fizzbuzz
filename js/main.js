8ada59b749bc01c074ffbc82114a36cf85a14757
Vale-riad <valentinariad91@gmail.com>
2022-10-24 16:18:35 +0200

    Aggiunge soluzione corretta

--- js/main.js
+++ js/main.js
@@ -1,12 +1,11 @@
 "use strict";
 // 1. stampare in console i numeri da 1 a 100
 for (let i = 1; i <= 100; i++) {
-    console.log(i);
-    if ( i % 3 === 0){
-        console.log("fizz");
-    } else if ( i % 5 === 0){
-    console.log("Buzz")
-    }if ( i % 15 === 0) {
-        console.log("FizzBuzz");
-    }
+    // 2.nei multipli di 3 viene stampato "Fizz"
+    if (i % 3 === 0) console.log("Fizz");
+    // 3.nei multipli di 5 viene stampato "Buzz"
+    else if (i % 5 === 0) console.log("Buzz");
+    // 4. nei multipli di 5 viene stampato "FizzBuzz"
+    else if (i % 15 === 0) console.log("FizzBuzz");
+    else console.log(i);
 }
