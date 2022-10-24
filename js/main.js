a439f0afce346078255c60e4d54c2a0f3d149548
Vale-riad <valentinariad91@gmail.com>
2022-10-24 16:04:50 +0200

    Aggiunge divisione per 3/5/15

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
 "use strict";
+// 1. stampare in console i numeri da 1 a 100
+for (let i = 1; i <= 100; i++) {
+    console.log(i);
+    if ( i % 3 === 0){
+        console.log("fizz");
+    } else if ( i % 5 === 0){
+    console.log("Buzz")
+    }if ( i % 15 === 0) {
+        console.log("FizzBuzz");
+    }
+}
