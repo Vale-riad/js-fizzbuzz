2b0db501e4b38c789ecf6f0d5da3b57b82a825a9
Vale-riad <valentinariad91@gmail.com>
2022-10-24 17:12:51 +0200

    Stampa contenuto nella pagina html

--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
 "use strict";
+const div = document.getElementById("numbers");
 // 1. stampare in console i numeri da 1 a 100
 for (let i = 1; i <= 100; i++) {
+    const element = document.createElement('div');
+    element.innerHTML = i;
+    div.append(element);
     // 2.nei multipli di 3 viene stampato "Fizz"
-    if (i % 3 === 0) console.log("Fizz");
+    if (i % 3 === 0) 
+    element.innerHTML ="Fizz"
     // 3.nei multipli di 5 viene stampato "Buzz"
-    else if (i % 5 === 0) console.log("Buzz");
+    else if (i % 5 === 0) 
+    element.innerHTML ="Buzz"
     // 4. nei multipli di 5 viene stampato "FizzBuzz"
-    else if (i % 15 === 0) console.log("FizzBuzz");
+    else if (i % 15 === 0) 
+    element.innerHTML ="FizzBuzz"
     else console.log(i);
 }
+
